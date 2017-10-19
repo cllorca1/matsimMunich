795929dcd452185c03c59637f89d81850b2feba4
cllorca1 <carlos.llorca@tum.de>
2017-10-19 09:46:53 +0200

    read headers properly

--- src/main/java/org/matsim/munichArea/configMatsim/planCreation/CentroidsToLocations.java
+++ src/main/java/org/matsim/munichArea/configMatsim/planCreation/CentroidsToLocations.java
@@ -1,6 +1,8 @@
 package org.matsim.munichArea.configMatsim.planCreation;
 
 
+import org.apache.log4j.Logger;
+
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
@@ -12,7 +14,15 @@ import java.util.ResourceBundle;
  */
 public class CentroidsToLocations {
 
-    ResourceBundle rb;
+    private ResourceBundle rb;
+    private static Logger logger = Logger.getLogger(CentroidsToLocations.class);
+
+    private int posId;
+    private int posX;
+    private int posY;
+    private int posPop;
+    private int posEmp;
+    private int posSize;
 
     public CentroidsToLocations(ResourceBundle rb) {
         this.rb = rb;
@@ -32,6 +42,15 @@ public class CentroidsToLocations {
             String line;
             bufferReader = new BufferedReader(new FileReader(fileName));
 
+            String headerLine = bufferReader.readLine();
+            String[] header = headerLine.split("\\s*,\\s*");
+
+            posId = findPositionInArray("id", header);
+            posX = findPositionInArray("x", header);
+            posY = findPositionInArray("y", header);
+            posPop = findPositionInArray("population", header);
+            posEmp = findPositionInArray("employment", header);
+            posSize = findPositionInArray("size", header);
 
             while ((line = bufferReader.readLine()) != null ) {
                 Location location = CSVtoLocation(line);
@@ -45,15 +64,15 @@ public class CentroidsToLocations {
         } finally {
             try {
                 if (bufferReader != null) bufferReader.close();
-            } catch (IOException crunchifyException) {
-                crunchifyException.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
             }
         }
 
         return locationList;
 
     }
-    public static Location CSVtoLocation(String csvLine) {
+    public Location CSVtoLocation(String csvLine) {
         int id;
         double x;
         double y;
@@ -64,17 +83,26 @@ public class CentroidsToLocations {
 
         try {
             //String[] splitData = csvLine.split(",");
-            id = Integer.parseInt(splitData[0]);
-            x = Double.parseDouble(splitData[1]);
-            y = Double.parseDouble(splitData[2]);
-            pop = Long.parseLong(splitData[3]);
-            emp = Long.parseLong(splitData[4]);
-            size = Float.parseFloat(splitData[5]);
-            Location location = new Location(id, x, y, pop, emp, size);
-            return location;
+            id = Integer.parseInt(splitData[posId]);
+            x = Double.parseDouble(splitData[posX]);
+            y = Double.parseDouble(splitData[posY]);
+            pop = Long.parseLong(splitData[posPop]);
+            emp = Long.parseLong(splitData[posEmp]);
+            size = Float.parseFloat(splitData[posSize]);
+
+            return new Location(id, x, y, pop, emp, size);
         } catch (Exception e){
             return null;
         }
+    }
+
+    public static int findPositionInArray(String element, String[] arr) {
+        // return index position of element in array arr
+        int ind = -1;
+        for (int a = 0; a < arr.length; a++) if (arr[a].equalsIgnoreCase(element)) ind = a;
+        if (ind == -1) logger.error("Could not find element " + element +
+                " in array (see method <findPositionInArray> in class <SiloUtil>");
+        return ind;
     }
 
 
