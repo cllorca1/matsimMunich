5e0f8e9fc70b04012f84b663c07184d0b017e432
cllorca1 <carlos.llorca@tum.de>
2017-10-26 16:41:08 +0200

    format changes

--- src/main/java/org/matsim/munichArea/RunMATSim.java
+++ src/main/java/org/matsim/munichArea/RunMATSim.java
@@ -6,18 +6,12 @@ import org.matsim.api.core.v01.Id;
 import org.matsim.api.core.v01.population.Population;
 import org.matsim.core.utils.geometry.transformations.TransformationFactory;
 import org.matsim.munichArea.configMatsim.MatsimRunFromJava;
-import org.matsim.munichArea.configMatsim.createDemandPt.MatsimGravityModel;
 import org.matsim.munichArea.configMatsim.createDemandPt.MatsimPopulationCreator;
 import org.matsim.munichArea.configMatsim.createDemandPt.PtSyntheticTraveller;
-import org.matsim.munichArea.configMatsim.createDemandPt.ReadZonesServedByTransit;
 import org.matsim.munichArea.configMatsim.planCreation.CentroidsToLocations;
 import org.matsim.munichArea.configMatsim.planCreation.Location;
 import org.matsim.munichArea.configMatsim.planCreation.ReadSyntheticPopulation;
-import org.matsim.munichArea.outputCreation.EuclideanDistanceCalculator;
 import org.matsim.munichArea.outputCreation.TravelTimeMatrix;
-import org.matsim.munichArea.outputCreation.accessibilityCalculator.Accessibility;
-import org.matsim.munichArea.outputCreation.transitSkim.TransitSkimCreator;
-
 import java.io.File;
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -33,15 +27,9 @@ public class RunMATSim {
         File propFile = new File(args[0]);
         rb = ResourceUtil.getPropertyBundle(propFile);
 
-        boolean createNetwork = ResourceUtil.getBooleanProperty(rb, "create.network");
-        boolean runMatsim = ResourceUtil.getBooleanProperty(rb, "run.matsim");
-        boolean runGravityModel = ResourceUtil.getBooleanProperty(rb, "run.gravity.model");
+
         boolean autoTimeSkims = ResourceUtil.getBooleanProperty(rb, "skim.auto.times");
         boolean autoDistSkims = ResourceUtil.getBooleanProperty(rb, "skim.auto.dist");
-        boolean ptSkimsFromEvents = ResourceUtil.getBooleanProperty(rb, "skim.pt.events");
-        boolean eucliddistSkims = ResourceUtil.getBooleanProperty(rb, "skim.eucliddist");
-        boolean analyzeAccessibility = ResourceUtil.getBooleanProperty(rb, "analyze.accessibility");
-        boolean visualize = ResourceUtil.getBooleanProperty(rb, "run.oftvis");
         String networkFile = rb.getString("network.folder") + rb.getString("xml.network.file");
         String scheduleFile = rb.getString("network.folder") + rb.getString("schedule.file");
         String vehicleFile = rb.getString("network.folder") + rb.getString("vehicle.file");
@@ -56,26 +44,20 @@ public class RunMATSim {
         CentroidsToLocations centroidsToLocations = new CentroidsToLocations(rb);
         ArrayList<Location> locationList = centroidsToLocations.readCentroidList();
 
-        //to make test reduce the size
-        /*ArrayList<Location> shortLocationList = new ArrayList<>();
-        shortLocationList.addAll(locationList.subList(0, 200));
-        locationList = shortLocationList;
-        System.out.println(locationList.size());*/
 
-        //get arrays of parameters for single runs
+        //get parameters for single run
         double[] tripScalingFactorVector = ResourceUtil.getDoubleArray(rb, "trip.scaling.factor");
         double tripScalingFactor = tripScalingFactorVector[0];
         int[] lastIterationVector = ResourceUtil.getIntegerArray(rb, "last.iteration");
         int iterations = lastIterationVector[0];
-
-        //initialize matrices
-        Matrix autoTravelTime = new Matrix(locationList.size(), locationList.size());
-        Matrix autoTravelDistance = new Matrix(locationList.size(), locationList.size());
-
         double flowCapacityExponent = Double.parseDouble(rb.getString("cf.exp"));
         double stroageFactorExponent = Double.parseDouble(rb.getString("sf.exp"));
 
+        //initialize auto skim matrices
+        Matrix autoTravelTime = new Matrix(locationList.size(), locationList.size());
+        Matrix autoTravelDistance = new Matrix(locationList.size(), locationList.size());
 
+        //calculate capacity factors
         double flowCapacityFactor = Math.pow(tripScalingFactor, flowCapacityExponent);
         System.out.println("Starting MATSim simulation. Sampling factor = " + tripScalingFactor);
         double storageCapacityFactor = Math.pow(tripScalingFactor, stroageFactorExponent);
@@ -86,9 +68,7 @@ public class RunMATSim {
 
 
         //alternative methods --> alternative gravity model that doesn't create other than a OD matrix with counts
-
         Population matsimPopulation;
-        Map<Id, PtSyntheticTraveller> ptSyntheticTravellerMap = new HashMap<>();
 
         //two alternative methods to create the demand
         if (useSp) {
@@ -104,11 +84,9 @@ public class RunMATSim {
             MatsimPopulationCreator matsimPopulationCreator = new MatsimPopulationCreator(rb);
             matsimPopulationCreator.createMatsimPopulation(locationList, 2013, false, tripScalingFactor);
             matsimPopulation = matsimPopulationCreator.getMatsimPopulation();
-
         }
 
-
-        //get travel times and run Matsim
+        //run Matsim and get travel times
         MatsimRunFromJava matsimRunner = new MatsimRunFromJava(rb);
         matsimRunner.runMatsim(hourOfDay * 60 * 60, 1,
                 networkFile, matsimPopulation, year,
@@ -119,11 +97,8 @@ public class RunMATSim {
         if (autoDistSkims) autoTravelDistance = matsimRunner.getAutoTravelDistance();
 
 
-        //visualization
-
-
+        //skim matrices if needed
         if (autoTimeSkims) {
-//                        String omxFileName = rb.getString("out.skim.auto.time") + simulationName + ".omx";
             String omxFileName = rb.getString("out.skim.auto.time") + singleRunName + ".omx";
             TravelTimeMatrix.createOmxSkimMatrix(autoTravelTime, locationList, omxFileName, "mat1");
 
@@ -132,7 +107,6 @@ public class RunMATSim {
             String omxFileName = rb.getString("out.skim.auto.dist") + simulationName + ".omx";
             TravelTimeMatrix.createOmxSkimMatrix(autoTravelDistance, locationList, omxFileName, "mat1");
         }
-
 
     }
 
