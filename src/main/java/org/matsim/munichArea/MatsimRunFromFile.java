dc3231bc37a33f6d61ecb6507a4b6652e77aaaa1
cllorca1 <carlos.llorca@tum.de>
2018-02-01 16:50:51 +0100

    testing synthetic schedule issues

--- src/main/java/org/matsim/munichArea/MatsimRunFromFile.java
+++ src/main/java/org/matsim/munichArea/MatsimRunFromFile.java
@@ -3,6 +3,8 @@ package org.matsim.munichArea;
 import org.matsim.api.core.v01.Scenario;
 import org.matsim.core.config.Config;
 import org.matsim.core.config.ConfigUtils;
+import org.matsim.core.config.groups.PlanCalcScoreConfigGroup;
+import org.matsim.core.config.groups.StrategyConfigGroup;
 import org.matsim.core.controler.Controler;
 import org.matsim.core.controler.OutputDirectoryHierarchy;
 import org.matsim.core.scenario.ScenarioUtils;
@@ -20,6 +22,27 @@ public class MatsimRunFromFile {
 
         //modify configuration parameters
         config.controler().setOverwriteFileSetting(OutputDirectoryHierarchy.OverwriteFileSetting.deleteDirectoryIfExists);
+
+        /*//add strategy:
+        StrategyConfigGroup.StrategySettings strategySettings3 = new StrategyConfigGroup.StrategySettings();
+        strategySettings3.setStrategyName("TimeAllocationMutator");
+        strategySettings3.setWeight(1); //originally 0
+        strategySettings3.setDisableAfter((int) (config.controler().getLastIteration() * 0.7));
+        config.strategy().addStrategySettings(strategySettings3);
+
+        //rename input folder:
+        config.controler().setOutputDirectory(config.controler().getOutputDirectory() + "withDefiningActivity");
+
+        //add the opening time, closing time and duration of activity
+        PlanCalcScoreConfigGroup.ActivityParams workActivity = new PlanCalcScoreConfigGroup.ActivityParams("work");
+        workActivity.setTypicalDuration(9 * 60 * 60);
+        workActivity.setLatestStartTime(8 * 60 * 60);
+        workActivity.setEarliestEndTime(17 *60 * 60);
+        workActivity.setClosingTime(17 * 60*60) ;
+        workActivity.setOpeningTime(8*60*60);
+        workActivity.setMinimalDuration(9*60*60);
+        config.planCalcScore().addActivityParams(workActivity);
+*/
 
         //load the scenario from the configuration settings
         Scenario scenario = ScenarioUtils.loadScenario(config);
---
+++ src/main/java/org/matsim/munichArea/configMatsim/planCreation/scheduleTests/ReadSyntheticScheduleTests.java
@@ -0,0 +1,122 @@
+package org.matsim.munichArea.configMatsim.planCreation.scheduleTests;
+
+import com.pb.common.util.ResourceUtil;
+import org.apache.log4j.Logger;
+import org.matsim.api.core.v01.Coord;
+import org.matsim.api.core.v01.Id;
+import org.matsim.api.core.v01.Scenario;
+import org.matsim.api.core.v01.TransportMode;
+import org.matsim.api.core.v01.population.*;
+import org.matsim.core.config.Config;
+import org.matsim.core.config.ConfigUtils;
+import org.matsim.core.scenario.ScenarioUtils;
+import org.matsim.munichArea.Util;
+import org.matsim.munichArea.configMatsim.planCreation.CentroidsToLocations;
+import org.matsim.munichArea.configMatsim.planCreation.Location;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.ResourceBundle;
+
+public class ReadSyntheticScheduleTests {
+
+
+    private static Logger logger = Logger.getLogger(ReadSyntheticScheduleTests.class);
+    private static String folder = "C:/models/matsimScheduleTests/";
+    private static String csvPlans = "input/subPopulation.csv";
+    private static String xmlPlans = "input/populationWorkSchedule.xml";
+    private ArrayList<Location> locationList;
+    private static ResourceBundle rb;
+
+    public static void main(String[] args){
+
+        File propFile = new File(args[0]);
+        rb = ResourceUtil.getPropertyBundle(propFile);
+
+        ReadSyntheticScheduleTests readSyntheticScheduleTests = new ReadSyntheticScheduleTests();
+        readSyntheticScheduleTests.readZones();
+
+        Config config = ConfigUtils.createConfig();
+        Scenario scenario = ScenarioUtils.createScenario(config);
+        Population matsimPopulation = scenario.getPopulation();
+
+        matsimPopulation = readSyntheticScheduleTests.createPlans(matsimPopulation, false,
+                true,true, false);
+
+        PopulationWriter populationWriter = new PopulationWriter(matsimPopulation);
+        populationWriter.write(folder + xmlPlans);
+
+    }
+
+    public void readZones(){
+        CentroidsToLocations centroidsToLocations = new CentroidsToLocations(rb);
+        locationList = centroidsToLocations.readCentroidList();
+    }
+
+    public Population createPlans(Population matsimPopulation,
+                                  boolean giveDepartureFromHome, boolean giveArrivalAtWork, boolean giveDepartureFromWork, boolean gideArrivalAtHome) {
+
+        PopulationFactory populationFactory = matsimPopulation.getFactory();
+        BufferedReader br = null;
+
+        try {
+            br = new BufferedReader(new FileReader(folder + csvPlans));
+
+
+
+        String[] header = br.readLine().split(",");
+        int positionId = Util.findPositionInArray("id", header);
+        int positionHomeZone = Util.findPositionInArray("homeZone", header);
+        int positionWorkZone = Util.findPositionInArray("zone", header);
+        int positionDepHW = Util.findPositionInArray("depHW", header);
+        int positionArrHW = Util.findPositionInArray("arrHW", header);
+        int positionDepWH = Util.findPositionInArray("depWH", header);
+        int positionArrWH = Util.findPositionInArray("arrWH", header);
+
+
+        String line;
+        while ((line = br.readLine()) != null ) {
+
+            Person matsimPerson = populationFactory.createPerson(Id.createPersonId(line.split(",")[positionId]));
+            Plan matsimPlan = populationFactory.createPlan();
+            int homeZone = Integer.parseInt(line.split(",")[positionHomeZone]);
+            int workZone = Integer.parseInt(line.split(",")[positionWorkZone]);
+
+            Coord homeCoord = new Coord(locationList.get(homeZone).getX(), locationList.get(homeZone).getY());
+            Coord workCoord = new Coord(locationList.get(workZone).getX(), locationList.get(workZone).getY());
+
+            Activity homeActivity = populationFactory.createActivityFromCoord("home", homeCoord);
+            Activity workActivity = populationFactory.createActivityFromCoord("work", workCoord);
+            Activity lateHomeActivity = populationFactory.createActivityFromCoord("home", homeCoord);
+
+            if (giveDepartureFromHome) homeActivity.setEndTime(Double.parseDouble(line.split(",")[positionDepHW]));
+            if (giveArrivalAtWork) workActivity.setEndTime(Double.parseDouble(line.split(",")[positionDepWH]));
+
+            if (giveDepartureFromWork) workActivity.setStartTime(Double.parseDouble(line.split(",")[positionArrHW]));
+            if (gideArrivalAtHome) lateHomeActivity.setStartTime(Double.parseDouble(line.split(",")[positionArrWH]));
+
+
+            matsimPlan.addActivity(homeActivity);
+            matsimPlan.addLeg(populationFactory.createLeg(TransportMode.car));
+            matsimPlan.addActivity(workActivity);
+            matsimPlan.addLeg(populationFactory.createLeg(TransportMode.car));
+            matsimPlan.addActivity(lateHomeActivity);
+
+            matsimPerson.addPlan(matsimPlan);
+            matsimPopulation.addPerson(matsimPerson);
+        };
+
+        br.close();
+
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return matsimPopulation;
+
+    }
+
+}
